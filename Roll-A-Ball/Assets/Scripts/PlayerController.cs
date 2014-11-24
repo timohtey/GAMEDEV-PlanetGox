936cae7011161bd6f144ee5ea442185f7a8ccf4c
Timothy Rodriguez <timohtey.rodriguez136@gmail.com>
2014-11-25 01:27:12 +0800

    Initial commit

---
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	private int count;
+
+	void Start () {
+		count = 0;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count++;
+		}
+	}                                                     
+
+}
