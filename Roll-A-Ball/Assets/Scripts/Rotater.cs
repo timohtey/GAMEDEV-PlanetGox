d9972a3e355042dea7018ee04ea6ac105402f5b9
mcanjelo <mcanjelo29@yahoo.com>
2014-12-03 10:07:18 +0800

    Item Spawn and Collection
    Spawning bag and umbrella
    No feedback yet for item collection

--- Roll-A-Ball/Assets/Scripts/Rotater.cs
+++ Roll-A-Ball/Assets/Scripts/Rotater.cs
@@ -1,10 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Rotater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 90, 0) * Time.deltaTime);
 	}
 }
---
+++ Roll-A-Ball/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Spawner : MonoBehaviour {
+
+	public float[] Xspawnpoint;
+	public float[] Yspawnpoint;
+	public float[] Zspawnpoint;
+	private int number;
+	private Vector3 spawnpoint;
+	// Use this for initialization
+	void Start () {
+		Random.seed = (int) System.DateTime.Now.Ticks;
+		number = (int)(Random.Range(0, 5));
+		Debug.Log (number);
+		spawnpoint = new Vector3 (Xspawnpoint [number], Yspawnpoint [number], Zspawnpoint [number]);
+		Debug.Log (spawnpoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = spawnpoint;
+	}
+}
