308f2e399181ade42e1f23b308fe04196baca774
bigbau <julius_bautista@rocketmail.com>
2014-12-14 02:17:43 +0800

    Working Start Game Button

---
+++ Roll-A-Ball/Assets/Scripts/GameMenu.js
@@ -0,0 +1,28 @@
+﻿var isStartGame=false;
+var isInstructions=false;
+
+function OnMouseEnter(){
+	renderer.transform.localScale += 3;
+}
+
+function OnMouseExit(){
+
+}
+
+function OnMouseUp(){
+
+	if (isStartGame==true) {
+		Application.LoadLevel(1);
+	}
+	else if (isInstructions==true) {
+	
+		Application.Quit();
+	}
+}
+
+function Update(){
+//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) { 
+		Application.Quit();
+	}
+}
